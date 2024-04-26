f849362801608e56be8aa3be65140e0895f147e8
JTtNinjaCode <ffa123654789@gmail.com>
2024-04-26 21:53:47 +0800

    fix:add camera component.

---
+++ genesis/core/component/camera.cpp
@@ -0,0 +1,46 @@
+#include "camera.h"
+
+#include <glm/gtx/transform.hpp>
+
+#include "core/component/game_object.h"
+#include "core/component/transform.h"
+namespace genesis {
+using namespace rttr;
+RTTR_REGISTRATION { registration::class_<Camera>("Camera").constructor<>()(rttr::policy::ctor::as_std_shared_ptr); }
+
+glm::mat4 Camera::GetProjection() const {
+  switch (projection_mode_) {
+    case ProjectionMode::kPerspective:
+      return perspective_;
+      break;
+    case ProjectionMode::kOrthographic:
+      return orthographics_;
+      break;
+  }
+}
+glm::mat4 Camera::GetView() const { return view_; }
+ProjectionMode genesis::Camera::GetProjectionMode() const { return projection_mode_; }
+void genesis::Camera::SetProjectionMode(ProjectionMode projection_mode) { projection_mode_ = projection_mode; }
+float genesis::Camera::GetFov() const { return fov_; }
+void Camera::SetFov(float fov) {
+  fov_ = fov;
+  RecalculatePerspectiveMatrix();
+}
+float genesis::Camera::GetSize() const { return size_; }
+void genesis::Camera::SetSize(float size) {
+  size_ = size;
+  RecalculateOrthographicMatrix();
+}
+
+void genesis::Camera::RecalculatePerspectiveMatrix() {
+  perspective_ = glm::perspective(fov_, 1.0f, clipping_plane_near_, clipping_plane_far_);
+}
+void genesis::Camera::RecalculateOrthographicMatrix() {
+  orthographics_ = glm::orthoRH(-size_, size_, -size_, size_, clipping_plane_near_, clipping_plane_far_);
+}
+void genesis::Camera::RecalculateViewMatrix() {
+  auto transform = dynamic_cast<const Transform*>(GetGameObject()->GetComponent("Transform"));
+  auto position = transform->position();
+  view_ = glm::lookAtRH(position, position + direction_, glm::vec3(0, 1, 0));
+}
+}  // namespace genesis
---
+++ genesis/core/component/camera.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <rttr/registration.h>
+
+#include <glm/glm.hpp>
+
+#include "core/component/component.h"
+namespace genesis {
+enum ProjectionMode { kPerspective, kOrthographic };
+
+class Camera : public Component {
+ public:
+  Camera() = default;
+  ProjectionMode camera_usage;
+  glm::mat4 GetProjection() const;
+  glm::mat4 GetView() const;
+  ProjectionMode GetProjectionMode() const;
+  void SetProjectionMode(ProjectionMode projection_mode);
+  float GetFov() const;
+  void SetFov(float fov);
+  float GetSize() const;
+  void SetSize(float size);
+
+  //bool OnEvent();
+
+ private:
+  void RecalculatePerspectiveMatrix();
+  void RecalculateOrthographicMatrix();
+  void RecalculateViewMatrix();
+  glm::vec3 clear_color_ = glm::vec3(255, 0, 144);
+  ProjectionMode projection_mode_ = kPerspective;
+  float fov_ = 0.0f;
+  float size_ = 0.0f;
+  float clipping_plane_near_ = 0.1f;
+  float clipping_plane_far_ = 100.0f;
+  glm::vec3 direction_ = glm::vec3(0.0f, 0.0f, 1.0f);
+  glm::mat4 perspective_;
+  glm::mat4 orthographics_;
+  glm::mat4 view_;
+};
+}  // namespace genesis
